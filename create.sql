b44a5fe1ac8a2ac7aeaab18083b04c61c68ece6a
Artemii <angap4@gmail.com>
2023-12-30 21:53:25 +1000

    init push

--- create.sql
+++ create.sql
@@ -5,6 +5,9 @@ DROP TABLE IF EXISTS Groups;
 DROP VIEW IF EXISTS Group_merge;
 DROP TABLE IF EXISTS Students;
 DROP TABLE IF EXISTS Activity;
+DROP TABLE IF EXISTS Disciplines;
+DROP TABLE IF EXISTS Subjects;
+DROP TABLE IF EXISTS Grades;
 
 CREATE TABLE Teachers
 (
@@ -52,9 +55,9 @@ CREATE TABLE Students
 
 CREATE TABLE Activity
 (
-    ID_student  INTEGER                                 NOT NULL,
-    Date_active date                                    NOT NULL,
-    ID_group    varchar(14)                             NOT NULL,
+    ID_student  INTEGER                              NOT NULL,
+    Date_active date                                 NOT NULL,
+    ID_group    varchar(14)                          NOT NULL,
     Status      INTEGER(1) CHECK ( Status IN (0, 1)) NOT NULL,
     PRIMARY KEY (ID_student, ID_group, Date_active),
     FOREIGN KEY (ID_group) REFERENCES Group_merge (ID_group),
@@ -63,10 +66,37 @@ CREATE TABLE Activity
 
 CREATE TABLE Disciplines
 (
-
+    Name        varchar(100) NOT NULL UNIQUE PRIMARY KEY,
+    Description text
 );
 
 CREATE TABLE Subjects
 (
+    Discipline varchar(100)                                             NOT NULL,
+    Group_name varchar(100)                                             NOT NULL,
+    -- Для удобства будет лучше сразу сделать семестр.
+    Date_year  INTEGER(4)                                               NOT NULL,
+    Date_sem   INTEGER(1) CHECK ( Date_sem IN (1, 2, 3, 4, 5, 6, 7, 8)) NOT NULL,
+    Grade_type INTEGER(1) CHECK ( Grade_type IN (1, 2))                 NOT NULL,
+    Teacher    INTEGER,
+    PRIMARY KEY (Discipline, Group_name, Date_year, Date_sem),
+    FOREIGN KEY (Discipline) REFERENCES Disciplines (Name),
+    FOREIGN KEY (Group_name) REFERENCES Group_merge (ID_group),
+    FOREIGN KEY (Teacher) REFERENCES Teachers (ID)
+);
 
-)
+-- Необходимо определить, как связать оценки и предмет.
+-- Может, будет лучше связать их с дисциплиной?
+-- Или просто добавить доп поля. Думаю, так будет лучше.
+
+CREATE TABLE Grades
+(
+    Student    INTEGER      NOT NULL,
+    Group_name varchar(14)  NOT NULL,
+    Discipline varchar(100) NOT NULL,
+    Date_year  INTEGER(4)   NOT NULL,
+    Date_sem   INTEGER(1)   NOT NULL,
+    FOREIGN KEY (Student) REFERENCES Students (ID_certificate),
+    FOREIGN KEY (Discipline, Group_name, Date_year, Date_sem) REFERENCES Subjects (Discipline, Group_name, Date_year, Date_sem)
+    -- Насколько необходимо постоянно писать NotNull, если в ForeignKey перечислены сразу все строки...
+);
