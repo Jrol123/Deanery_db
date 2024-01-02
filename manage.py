61159ffa0497cb340448f3cd1f134a2eace74be4
Artemii <angap4@gmail.com>
2024-01-02 15:07:40 +1000

    Update manage.py

--- manage.py
+++ manage.py
@@ -25,37 +25,72 @@ def clear_console():
 def insert_teacher() -> None:
     """
 
-    :param full_name: Полное имя учителя. 200 символов
-    :type full_name: str
-    :param gender: Пол учителя. m, f, o
-    :type gender: str
-    :param has_degree: Наличие учёной степени
-    :type has_degree: bool
-    :param date_birth: Дата рождения. "2000-01-01"
+    Создание учителя.
 
-    :return: Создаёт запись типа teacher
+    :return: Создаёт запись в таблицу Teachers
     :rtype: None
 
     """
     state_input = ""
     full_name = ""
+    """Полное имя. 200 символов"""
     gender = ""
+    """Пол. m, f, o"""
     has_degree = False
+    """Наличие учёной степени"""
     date_birth = ""
+    """Дата рождения. '2000-01-01'"""
+
     while state_input != "y":
         clear_console()
         full_name = input("Введите полное ФИО:\n    ")
         gender = input("Введите пол ([m]ale, [f]emale, [o]ther:\n    ")
-        has_degree = True if input("Имеется ли научная степень? ([y]es, [n]o:\n    ") == "y" else False
+        has_degree = True if input("Имеется ли научная степень? [y]es, [n]o:\n    ") == "y" else False
         date_birth = input("Введите дату рождения в формате: '2000-01-01':\n    ")
 
         print(f"Проверьте правильность введённых данных:\n"
               f"    {full_name}\n    {gender}\n    {has_degree}\n    {date_birth}")
         state_input = input("\nВсё верно? [y]es, [n]o:\n    ")
         "Специально не проверяю в самом коде правильность ввода, чтобы было видно, что всё правильно обрабатывается в SQL"
+
     "Try-Except не работает..."
     cursor.execute("INSERT INTO Teachers ('Full Name', gender, has_degree, 'Date of birth') VALUES (?, ?, ?, ?)",
-               (full_name, gender, has_degree, date_birth))
+                   (full_name, gender, has_degree, date_birth))
+
+
+def insert_student() -> None:
+    """
+
+    Создание студента.
+
+    :return: Создаёт запись в таблицу Students
+    :rtype: None
+
+    """
+
+    state_input = ""
+    full_name = ""
+    """Полное имя. 200 символов"""
+    gender = ""
+    """Пол. m, f, o"""
+    id_certificate = 0
+    """Номер аттестата."""
+    date_birth = ""
+    """Дата рождения. '2000-01-01'"""
+
+    while state_input != "y":
+        clear_console()
+        full_name = input("Введите полное ФИО:\n    ")
+        gender = input("Введите пол ([m]ale, [f]emale, [o]ther:\n    ")
+        id_certificate = int(input("Введите номер аттестата:\n    "))
+        date_birth = input("Введите дату рождения в формате: '2000-01-01':\n    ")
+
+        print(f"\n\nПроверьте правильность введённых данных:\n"
+              f"    {full_name}\n    {gender}\n    {id_certificate}\n    {date_birth}")
+        state_input = input("\nВсё верно? [y]es, [n]o:\n    ")
+
+    cursor.execute("INSERT INTO Students (ID_certificate, 'Full Name', Gender, 'Date of birth') VALUES (?, ?, ?, ?)",
+                   (id_certificate, full_name, gender, date_birth))
 
 
 if __name__ == "__main__":
@@ -72,7 +107,7 @@ if __name__ == "__main__":
             case "create teacher":
                 insert_teacher()
             case "create student":
-                pass
+                insert_student()
             case _:
                 is_success = False
         connection.commit()
